7b6cff22c7028c1966033c2aa140824fc0549bd7
Xuân Trường <alonesera1612@gmail.com>
2025-02-06 22:38:27 +0700

    Update init.lua

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- pull lazy vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+print("--- Xuân Trường ---")
+vim.opt.rtp:prepend(lazypath)
+
+-- install plugins and options
+require("vim-options")
+require("lazy").setup("plugins")
