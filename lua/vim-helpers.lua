315a745d22981ad7a77f2bf8319efa03138eaf0a
Xuân Trường <alonesera1612@gmail.com>
2025-02-07 16:11:54 +0700

    Create vim-helpers.lua

---
+++ lua/vim-helpers.lua
@@ -0,0 +1,24 @@
+-- all vim helper functions here
+
+vim.keymap.set("n", "<leader>ce", function()
+	local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+	if #diagnostics > 0 then
+		local message = diagnostics[1].message
+		vim.fn.setreg("+", message)
+		print("Copied diagnostic: " .. message)
+	else
+		print("No diagnostic at cursor")
+	end
+end, { noremap = true, silent = true })
+
+-- go to errors in a file :/
+vim.keymap.set("n", "<leader>ne", vim.diagnostic.goto_next) -- next err
+vim.keymap.set("n", "<leader>pe", vim.diagnostic.goto_prev) -- previous err
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+-- copy current file path (absolute) into clipboard
+vim.keymap.set("n", "<leader>cp", function()
+	local filepath = vim.fn.expand("%:p")
+	vim.fn.setreg("+", filepath) -- Copy to Neovim clipboard
+	vim.fn.system("echo '" .. filepath .. "' | pbcopy") -- Copy to macOS clipboard
+	print("Copied: " .. filepath)
+end, { desc = "Copy absolute path to clipboard" })
