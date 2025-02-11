0270e67638fdc95905ff3a0c537e723c22001161
Xuân Trường <alonesera1612@gmail.com>
2025-02-11 22:52:01 +0700

    last update

--- lua/plugins/pomo.lua
+++ lua/plugins/pomo.lua
@@ -10,21 +10,23 @@ return {
         config = function()
             local play_sound = function()
                 local home_dir = os.getenv("HOME")
-                local sound_path = home_dir .. "/.config/nvim/break.wav"  -- đổi sang .wav nếu sử dụng paplay
+                local sound_path = home_dir .. "/.config/nvim/break.wav"  -- Đổi sang .wav nếu sử dụng paplay
 
-                -- Sử dụng paplay (PulseAudio) để phát âm thanh
+                -- Chạy paplay (PulseAudio) để phát âm thanh
                 vim.loop.spawn("paplay", {
                     args = { sound_path },
                     detached = true,
                 }, function()
                     print("Sound finished playing")
                 end)
-
+                
+                -- Sau khi thời gian kết thúc, tắt âm thanh
                 vim.defer_fn(function()
-                    -- Không cần phải kết thúc khi dùng paplay vì nó tự động dừng
-                    print("Sound played")
-                end, 15000)
+                    vim.fn.system("pkill paplay")  -- Tắt tất cả tiến trình paplay
+                    print("Sound stopped after time is up")
+                end, 15000)  -- Tạm dừng 15 giây (thời gian để chắc chắn âm thanh đã kết thúc)
             end
+
             require("notify").setup({
                 background_colour = "#1e1e2e",
                 on_open = function(notification)
@@ -33,6 +35,7 @@ return {
                     end
                 end,
             })
+            
             require("lualine").setup {
                 sections = {
                     lualine_x = {
