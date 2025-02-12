14a1ed3f3ae275d2a3f2cd1b7135bdcccff4a938
Xuân Trường <alonesera1612@gmail.com>
2025-02-12 22:32:36 +0700

    Update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,19 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- Biểu tượng đẹp hơn
-    config = function()
-        require("lualine").setup{
-            options = {
-                section_separators = { left = "", right = "" },
-                component_separators = { left = "", right = "" },
-                theme = "tokyonight"
-            }
-        }
-    end
-}
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "folke/tokyonight.nvim" },
+  config = function()
+    -- Cấu hình tokyonight trước khi load Lualine
+    require("tokyonight").setup({})
+    vim.cmd("colorscheme tokyonight")
 
+    -- Cấu hình Lualine
+    require("lualine").setup {
+      options = {
+        theme = "tokyonight",
+        icons_enabled = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      }
+    }
+  end
+}
