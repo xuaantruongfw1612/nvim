4192faffd99c65a4706dc347ff11c865a4efa8fe
Xuân Trường <alonesera1612@gmail.com>
2025-02-12 22:39:37 +0700

    Update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,19 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "folke/tokyonight.nvim" },
-  config = function()
-    -- Cấu hình tokyonight trước khi load Lualine
-    require("tokyonight").setup({})
-    vim.cmd("colorscheme tokyonight")
+-- Đảm bảo đã cài đặt 'tokyonight' và 'lualine'
+require("tokyonight").setup({
+  transparent = true, -- Bật chế độ trong suốt
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  }
+})
 
-    -- Cấu hình Lualine
-    require("lualine").setup {
-      options = {
-        theme = "tokyonight",
-        icons_enabled = true,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-      }
-    }
-  end
-}
+require("lualine").setup({
+  options = {
+    theme = "tokyonight", -- Dùng theme Tokyonight cho Lualine
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
+  }
+})
+
+vim.opt.termguicolors = true  -- Bật hỗ trợ màu sắc tốt hơn
+vim.cmd("colorscheme tokyonight") -- Áp dụng theme Tokyonight
