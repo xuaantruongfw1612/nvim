62733a3b6daf00f10e853cfa969f91d0da9e1441
truong <alonesera1612@gmail.com>
2025-02-13 09:23:31 +0700

    update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,42 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "folke/tokyonight.nvim" }, -- Đảm bảo Lualine load sau khi theme có sẵn
-  config = function()
-    -- Cài đặt theme tokyonight
-    require("tokyonight").setup({
-            style = "moon",
-            transparent = true,
-            terminal_colors = true,
-        })
-    vim.cmd("colorscheme tokyonight")
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "moon",
+        transparent = true,  -- Giữ trong suốt
+        terminal_colors = true,
+      })
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
 
-    -- Cấu hình Lualine
-    require("lualine").setup {
-      options = {
-        theme = "tokyonight",
-        icons_enabled = true,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-      }
-    }
-  end
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "folke/tokyonight.nvim" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "tokyonight",
+          icons_enabled = true,
+          component_separators = { left = "│", right = "│" },
+          section_separators = { left = "", right = "" },
+        }
+      })
+    end
+  },
+
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    priority = 999,  -- Load ngay sau theme
+    config = function()
+      require("transparent").setup({
+        enable = true,
+        extra_groups = { "Normal", "NormalNC", "EndOfBuffer", "Lualine" }, -- Giữ cả lualine trong suốt
+      })
+    end
+  }
 }
+
