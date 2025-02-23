269ac7f3b4cc0ebabc70063e9392e7bca1f6a14a
truong <alonesera1612@gmail.com>
2025-02-23 05:06:59 +0000

    last update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,14 +4,15 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        -- null_ls.builtins.diagnostics.mypy,
-        -- null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.code_actions.impl,
+        null_ls.builtins.formatting.stylua, -- lua
+        null_ls.builtins.formatting.clang_format, -- c/cpp
+        null_ls.builtins.formatting.prettier, -- JS, HTML, CSS,...
+        null_ls.builtins.formatting.isort, -- python
+        null_ls.builtins.diagnostics.mypy, -- python
+        --null_ls.builtins.formatting.black, -- python
+        --null_ls.builtins.diagnostics.ruff, -- python modern
+        --null_ls.builtins.formatting.gofumpt,
+        --null_ls.builtins.code_actions.impl,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,26 +1,39 @@
 local M = {}
 
 vim.keymap.set("n", "<leader>t", function()
-	vim.cmd("w")
+    vim.cmd("w")
 
-	local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-	local cmd
+    local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+    local cmd
 
-	if filetype == "c" then
-		cmd = 'split | term gcc -o "%:r" "%" && ./"%:r"'
-	elseif filetype == "cpp" then
-		cmd = 'split | terminal g++ -o "%:r" "%" && ./"%:r"'
-	elseif filetype == "python" then
-		cmd = 'split | term python3 "%"'
-	elseif filetype == "java" then
-		cmd = 'split | term javac "%" && java "%:r"'
-	else
-		print("Không hỗ trợ biên dịch cho loại tệp này.")
-		return
-	end
+if filetype == "c" then
+    vim.cmd("vertical rightbelow vsplit")
+    vim.cmd("vertical resize 50")
+    cmd = 'term gcc -o "%:r" "%" && ./"%:r"'
+elseif filetype == "cpp" then
+    vim.cmd("vertical rightbelow vsplit")
+    vim.cmd("vertical resize 50")
+    cmd = 'term g++ -o "%:r" "%" && ./"%:r"'
+elseif filetype == "python" then
+    vim.cmd("vertical rightbelow vsplit")
+    vim.cmd("vertical resize 50")
+    cmd = 'term python3 "%"'
+elseif filetype == "java" then
+    vim.cmd("vertical rightbelow vsplit")
+    vim.cmd("vertical resize 50")
+    cmd = 'term javac "%" && java "%:r"'
+else
+    print("No support file =))")
+    return
+end
 
-	vim.cmd(cmd)
-	vim.cmd("startinsert") -- Insert mode
+    vim.cmd(cmd)
+    vim.cmd("startinsert") -- insert
+
+    -- keymap for terminal
+	vim.api.nvim_buf_set_keymap(0, "n", "<CR>", ":q<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "t", "<Up>", "<C-\\><C-n><Up>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "t", "<Down>", "<C-\\><C-n><Down>", { noremap = true, silent = true })
 end, { noremap = true, silent = true })
 
 return M
