f63c281b024879eee5f7be96a3e7010f015c051e
truong <alonesera1612@gmail.com>
2025-04-29 09:17:46 +0000

    update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -21,9 +21,44 @@ return {
 			require("lualine").setup({
 				options = {
 					theme = "tokyonight", -- Lấy màu từ theme
-					icons_enabled = true, -- Bật icon
-					component_separators = { left = "│", right = "│" }, -- Dấu phân cách giữa các phần tử
-					section_separators = { left = "", right = "" }, -- Dấu phân cách giữa các section
+					icons_enabled = true, -- icon
+					component_separators = { left = "│", right = "│" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = {
+						"mode", -- Chế độ sẽ thay đổi màu sắc tự động khi bạn chuyển chế độ
+					},
+					lualine_b = {
+						"branch",
+						{
+							"diagnostics",
+							sources = { "nvim_diagnostic" },
+							sections = { "error", "warn", "info", "hint" }, -- lỗi, cảnh báo, thông tin, gợi ý
+							symbols = { error = " ", warn = " ", info = " ", hint = " " },
+							colored = true, -- màu
+							update_in_insert = false, -- Không cập nhật thông báo khi insert mode
+							always_visible = true, -- Luôn hiển thị thông báo dù không có lỗi hay cảnh báo nào
+						},
+					},
+					lualine_c = {
+						{
+							"filename",
+							file_status = true, -- Chế độ lưu, readonly
+							path = 1, -- Đường dẫn ngắn gọn
+						},
+					},
+					lualine_x = {
+						function()
+							local ok, wttr = pcall(require, "wttr")
+							return ok and wttr.text or "Weather Unavailable"
+						end,
+						"encoding",
+						"fileformat",
+						"filetype",
+					},
+					lualine_y = { "progress" }, -- Tiến trình cuộn file
+					lualine_z = { "location" }, -- Vị trí dòng/cột
 				},
 			})
 		end,
@@ -37,10 +72,15 @@ return {
 		config = function()
 			require("transparent").setup({
 				enable = true, -- Bật chế độ trong suốt
-				extra_groups = { 
-					"Normal", "NormalNC", "EndOfBuffer", "Lualine" -- Các group được làm trong
+				extra_groups = {
+					"Normal",
+					"NormalNC",
+					"EndOfBuffer",
+					"Lualine", -- Các group được làm trong
 				},
 			})
 		end,
 	},
 }
+
+-- │        
