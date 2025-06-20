ef2455b7b4c6b37cc083c9bf1a7aa79e30c98ef7
truong <alonesera1612@gmail.com>
2025-06-20 08:24:24 +0700

    neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,26 +9,5 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>v", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>xx", ":Neotree filesystem close<CR>", {})
-
-		require("neo-tree").setup({
-			close_if_last_window = true,
-			popup_border_style = "rounded",
-
-			default_component_configs = {
-				git_status = {
-					symbols = {
-						added = "+",
-						modified = "~",
-						deleted = "x",
-						renamed = "→",
-						untracked = "?",
-						ignored = "◌",
-						unstaged = "!",
-						staged = "✓",
-						conflict = "⚔",
-					},
-				},
-			},
-		})
-	end,
+    end,
 }
